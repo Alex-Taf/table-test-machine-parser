39ef97ee3bbe65f1a333829ea7a6acbcda7b3713
Alex <alexxvenus@yandex.ru>
2022-01-03 17:30:19 +0000

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<main>
+  
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
